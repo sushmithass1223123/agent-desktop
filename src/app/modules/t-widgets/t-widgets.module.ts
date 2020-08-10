2cd4208ff4bc97955b08a229bcf95b9d410c7750
Mohammed Sirajuddin <sirajuddin.m@tetherfi.com>
2020-08-10 09:20:18 -0400

    Update t-widgets.module.ts

--- src/app/modules/t-widgets/t-widgets.module.ts
+++ src/app/modules/t-widgets/t-widgets.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '@modules/shared/shared.module';
-import { TwCollectionsModule } from './tw-collections/tw-collections.module';
-import { TwContentModule } from './tw-content/tw-content.module';
-import { TwTemplateModule } from './tw-template/tw-template.module';
-import { TwToolbarModule } from './tw-toolbar/tw-toolbar.module';
-import { TFlipDirective } from './utils/t-flip/t-flip.directive';
-
-const widgetModules = [TwToolbarModule, TwContentModule, TwTemplateModule, TwCollectionsModule];
+import { TwCardHeaderComponent } from './tw-card-header/tw-card-header.component';
+import { TwCardComponent } from './tw-card/tw-card.component';
+import { TwWrapperComponent } from './tw-wrapper.component';
 
 @NgModule({
-    declarations: [TFlipDirective],
-    imports: [SharedModule, ...widgetModules],
-    exports: [...widgetModules, TFlipDirective]
+    declarations: [
+        TwCardComponent,
+        TwCardHeaderComponent,
+
+        TwWrapperComponent
+    ],
+    imports: [
+        SharedModule
+    ],
+    exports: [
+        TwWrapperComponent
+    ]
 })
-export class TWidgetsModule {}
+export class TwWrapperModule { }
