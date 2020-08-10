c885382007ad6b262f434a25ea26b43d199aa996
Mohammed Sirajuddin <sirajuddin.m@tetherfi.com>
2020-08-10 09:18:20 -0400

    Delete t-flip.directive.spec.ts

--- src/app/modules/t-widgets/utils/t-flip/t-flip.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TFlipDirective } from './t-flip.directive';
-
-describe('TFlipDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TFlipDirective();
-    expect(directive).toBeTruthy();
-  });
-});
