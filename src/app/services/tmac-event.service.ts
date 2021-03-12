3960db74f9ed8a6155e242fdc588d876edc18f3e
Shaikh Mohd Rahil <rahil.shaikh@tetherfi.com>
2021-03-11 22:33:36 -0500

    Version 5.0.0303 - AD to have a feature to check the media device availability based on agent channels and then show indicator for that (mic, camera, screenshare) - AD email compose - Keep the html toolbar always visible - AD - Customer details widget maximized view to be enhanced (dont expand the data. Keep in small area)
    Former-commit-id: 5fa053585b3d7dbe22799fdc28a083a598d6aa2d

--- src/app/services/tmac-event.service.ts
+++ src/app/services/tmac-event.service.ts
@@ -123,7 +123,7 @@ export class TMACEventService {
         private _appUIService: AppUiService,
         private _aotWidgetService: AOTWidgetService,
         private _router: Router
-    ) { }
+    ) {}
 
     // -----------------------------------------------------------------------------------------------------
     // @ Accessors
@@ -150,7 +150,7 @@ export class TMACEventService {
         } else {
             this.processNonInteractionEvents(evt);
         }
-    }
+    };
 
     /**
      * To process interaction events
@@ -514,7 +514,7 @@ export class TMACEventService {
         } catch (error) {
             TUtils.Logger.error('Exception in AgentNotificaitonEvent', error);
         }
-    }
+    };
 
     /**
      * Remider action executed method to update agent reminder
@@ -524,7 +524,9 @@ export class TMACEventService {
         SDKClient.updateAgentReminder({
             id,
             message: '',
-            status
+            status,
+            reminderDate: '',
+            reminderTime: ''
         });
     }
 
@@ -539,7 +541,7 @@ export class TMACEventService {
             state: evt.ColorCode,
             duration: 10000
         });
-    }
+    };
 
     /**
      * To process HoldTimerEvent
@@ -558,7 +560,7 @@ export class TMACEventService {
             state: evt.ColorCode,
             onClick: redirectToInteraction
         });
-    }
+    };
 
     /**
      * To process Quiz Event
@@ -593,7 +595,7 @@ export class TMACEventService {
         widget.Data.Url = url.toString();
 
         this._aotWidgetService.addWidget(widget);
-    }
+    };
 
     /**
      * Tp process GenericInteractionEvent
@@ -640,7 +642,7 @@ export class TMACEventService {
         } else {
             this.promptTCMWQDACRequest(evt);
         }
-    }
+    };
 
     /**
      * To process TCM WQ DAC request
@@ -717,7 +719,7 @@ export class TMACEventService {
         this._remiderTaskDialog.tcmWQVoice = null;
         // close the generic interaction in server
         SDKClient.closeInteraction(evt.InteractionID.toString());
-    }
+    };
 
     /**
      * To process AgentReminderEvent
@@ -756,7 +758,7 @@ export class TMACEventService {
                 });
             }
         });
-    }
+    };
 
     /**
      * To process AgentForcedLogoffEvent
@@ -796,7 +798,7 @@ export class TMACEventService {
             // }
         });
         this._appUIService.showSnackbar(description);
-    }
+    };
 
     /**
      * To process TextChatTransferNotificationEvent
@@ -822,7 +824,7 @@ export class TMACEventService {
             .subscribe((resp1) => {
                 evt.Response(resp1);
             });
-    }
+    };
 
     /**
      * To process TmacServerConnectionSuccess
@@ -833,7 +835,7 @@ export class TMACEventService {
         this._appUIService.showAppSnackbar({
             message: `New TMAC server [(${evt.ResultMessage})] connection established`
         });
-    }
+    };
 
     // -----------------------------------------------------------------------------------------------------
     // @ Public Methods
