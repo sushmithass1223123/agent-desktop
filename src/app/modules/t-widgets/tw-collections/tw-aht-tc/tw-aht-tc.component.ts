1d74e3c0143bd3e97baa0950f05fbcb26411d9f3
Shaikh Mohd Rahil <rahil.shaikh@tetherfi.com>
2021-04-14 06:07:24 -0400

    FIX : - Active agents snackbar messages fixes - Event form / calendar date , validation fixes
    Former-commit-id: 39cc295c89ed7b1590f35c36ab12cb392a956ad9

--- src/app/modules/t-widgets/tw-collections/tw-aht-tc/tw-aht-tc.component.ts
+++ src/app/modules/t-widgets/tw-collections/tw-aht-tc/tw-aht-tc.component.ts
@@ -8,7 +8,6 @@ import { TWidgetWrapper } from '@twidgets/utils/widget-wrapper/tw-wrapper';
 import { CHART_COLORS } from 'app/constants';
 import { CustomSDKEvent, TwChartConfig } from 'app/interfaces';
 import { takeUntil } from 'rxjs/operators';
-import { AgentChannelDataList, SDKClient } from 'tmac-sdk';
 
 type Sources = 'dashboard' | 'supervisor';
 
@@ -110,16 +109,17 @@ export class TwAhtTcComponent extends TWidgetWrapper implements OnInit, OnDestro
         if (this.dataConfig.Source === 'dashboard') {
             // SDKClient.events.on('AgentChannelDetailsEvent', this.AgentChannelDetailsEvent);
 
-            this._tmacEventService.getEvents(['AgentChannelDetailsEvent'])
+            this._tmacEventService
+                .getEvents(['AgentChannelDetailsEvent'])
                 .pipe(takeUntil(this.unsubscribeAll))
-                .subscribe(evts => this.AgentChannelDetailsEvent(evts[0]));
-
+                .subscribe((evts) => this.AgentChannelDetailsEvent(evts[0]));
         } else if (this.dataConfig.Source === 'supervisor') {
             // SDKClient.events.on('TeamChannelListEvent', this.TeamChannelListEvent);
 
-            this._tmacEventService.getEvents(['TeamChannelListEvent'])
+            this._tmacEventService
+                .getEvents(['TeamChannelListEvent'])
                 .pipe(takeUntil(this.unsubscribeAll))
-                .subscribe(evts => this.TeamChannelListEvent(evts[0]));
+                .subscribe((evts) => this.TeamChannelListEvent(evts[0]));
         }
     }
 
@@ -142,14 +142,14 @@ export class TwAhtTcComponent extends TWidgetWrapper implements OnInit, OnDestro
 
     /**
      * AgentChannelDetailsEvent handler
-     * @param {CustomSDKEvent} evt 
+     * @param {CustomSDKEvent} evt
      */
     private AgentChannelDetailsEvent = (evt: CustomSDKEvent) => {
         this.interactionList = evt.Data.Channels;
         this.interactionDetailsTable.source = new MatTableDataSource(this.interactionList);
         this.interactionDetailsTable.source.sort = this.sort;
         this.interactionDetailsTable.source.paginator = this.paginator;
-    }
+    };
 
     // Methods for Source === 'dashboard' ::: End
 
@@ -157,22 +157,38 @@ export class TwAhtTcComponent extends TWidgetWrapper implements OnInit, OnDestro
 
     /**
      * TeamChannelListEvent handler
-     * @param {CustomSDKEvent} evt 
+     * @param {CustomSDKEvent} evt
      */
     private TeamChannelListEvent = (evt: CustomSDKEvent) => {
         const datasets = { AHT: [], 'Transfer / Conference': [] };
         const labels = [];
         evt.Data.Channels.forEach((c: any) => {
-            datasets.AHT.push(c.AverageActiveTime + c.AverageHoldTime);
-            datasets['Transfer / Conference'].push(c.Transfer + c.Conference);
+            /**
+             * Add only if data exists
+             */
+            if (c.AverageActiveTime + c.AverageHoldTime) {
+                datasets.AHT.push(c.AverageActiveTime + c.AverageHoldTime);
+            }
+            /**
+             * Add only if data exists
+             */
+            if (c.Transfer + c.Conference) {
+                datasets['Transfer / Conference'].push(c.Transfer + c.Conference);
+            }
             labels.push(c.Channel);
         });
-        this.ahtChart.datasets = Object.keys(datasets).map((d) => ({
-            data: datasets[d],
-            label: d
-        }));
+        this.ahtChart.datasets = Object.entries(datasets).reduce((acc, curr) => {
+            const [key, val] = curr;
+            /**
+             * Add dataset only if data exists
+             */
+            if (val.length) {
+                acc.push({ data: val, label: key });
+            }
+            return acc;
+        }, []);
         this.ahtChart.labels = labels;
-    }
+    };
 
     // Methods for Source === 'supervisor' ::: End
 }
