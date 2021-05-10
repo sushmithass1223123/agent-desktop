8f014ecac9c84da3b0bbc809fdb1dec03b20ee4f
sirajuddin.m <sirajuddin.m@tetherfi.com>
2021-05-10 11:25:17 +0530

    Version 5.0.510-beta.1 commit - When there is counter for chat interaction if we navigate to the chat from navbar it was not getting cleared issue fixed
    Former-commit-id: 971089b6faa6f4a625da789f330cf70b9685300f

--- src/app/modules/t-widgets/tw-collections/tw-chat-controls/tw-chat-controls.component.ts
+++ src/app/modules/t-widgets/tw-collections/tw-chat-controls/tw-chat-controls.component.ts
@@ -47,11 +47,12 @@ import { TWidgetWrapper } from '@twidgets/utils/widget-wrapper/tw-wrapper';
 import { INVALID_CHARS } from 'app/constants';
 import { AgentSkillListData, ChatTranscripts, CustomSDKEvent, InteractionComment, InteractionRef, IWidget, SnackbarStateTypes } from 'app/interfaces';
 import { TwWidgetModel } from 'app/models';
+import { urlify } from 'app/utils';
 import { map } from 'lodash';
 import * as moment from 'moment';
 import { from, Subject, timer } from 'rxjs';
 import { takeUntil } from 'rxjs/internal/operators/takeUntil';
-import { delay, filter } from 'rxjs/operators';
+import { catchError, delay, filter } from 'rxjs/operators';
 
 const holdState = { onHold: true, buttonTooltip: 'Unhold', icon: 'play_arrow', loading: false };
 const unHoldState = { onHold: false, buttonTooltip: 'Hold', icon: 'pause', loading: false };
@@ -432,10 +433,31 @@ export class TwChatControlsComponent extends TWidgetWrapper implements OnInit, O
         //         config.layout.anchorWidget.customBackgroundColor === true && this.data.Config.Anchor ? config.layout.anchorWidget.bodyBackground : '';
         // });
 
-        this._interactionManagerService.interactions.pipe(takeUntil(this.unsubscribeAll)).subscribe((interactions: InteractionRef[]) => {
-            // filter out the textchat interaction
-            this.interactionList = interactions.filter((i: InteractionRef) => i.type === 'textchat');
-        });
+        this._contentPageService.mode
+            .pipe(takeUntil(this.unsubscribeAll))
+            .subscribe((viewMode: string) => {
+                // check if textchat view and selected interaction is this
+                if (viewMode === this.data.Data.Path) {
+                    const interaction = this.interactionList?.filter(i => i.isActive && i.otherData.unreadCount > 0)?.[0];
+                    // check the interaction 
+                    if (interaction) {
+                        // update is unread count
+                        this._interactionManagerService.updateInteraction(interaction.interactionId, {
+                            otherData: {
+                                unreadCount: 0
+                            }
+                        });
+                    }
+                }
+            });
+
+        this._interactionManagerService
+            .interactions
+            .pipe(takeUntil(this.unsubscribeAll))
+            .subscribe((interactions: InteractionRef[]) => {
+                // filter out the textchat interaction
+                this.interactionList = interactions.filter((i: InteractionRef) => i.type === 'textchat');
+            });
 
         // set the user info
         this.user = SDKClient.getAgentData() || null;
@@ -1098,21 +1120,21 @@ export class TwChatControlsComponent extends TWidgetWrapper implements OnInit, O
             repliedToMessage: this.replyingToMessage
         });
 
-        // to show message alert
-        this._appUIService.showDesktopAlert('New Message', `Message from ${this.customerName}`, true, 'message');
-
         let isActive = false;
+
         // check if the interaction is active, else count unread
         this.interactionList.forEach((item: InteractionRef) => {
             isActive = item.interactionId === this.interactionId && item.isActive;
         });
 
         // in not active then increment the count
-        if (!isActive || this._contentPageService.getCurrentMode() !== this.data.Data.Path) {
+        if (!evt.RecoveryEvent && (!isActive || this._contentPageService.getCurrentMode() !== this.data.Data.Path)) {
             const currentInteraction = this.interactionList.filter((i) => i.interactionId === this.interactionId)[0];
             const unreadCount = ++currentInteraction.otherData.unreadCount;
+
             // play new chat sound
             this._appUIService.playAudio('message', 0.5, false);
+
             // update the interaction other data
             this._interactionManagerService.updateInteraction(evt.InteractionID, {
                 otherData: {
@@ -1127,8 +1149,8 @@ export class TwChatControlsComponent extends TWidgetWrapper implements OnInit, O
         // freeze auto response if needed
         this.freezeAutoResponse(false);
 
-        // TODO:: show chrome notification if needed
-        //        hide freeze auto response button
+        // to show message alert
+        this._appUIService.showDesktopAlert('New Message', `Message from ${this.customerName}`, true, 'message');
     }
 
     /**
@@ -1501,10 +1523,20 @@ export class TwChatControlsComponent extends TWidgetWrapper implements OnInit, O
                     }
                 } else {
                     this._appUIService.showSnackbar('Message send failed!', 'failure');
+                    this.chatTranscripts.map(t => {
+                        if (t.messageId === messageId) {
+                            t.status = 'failed';
+                        }
+                    });
                 }
             })
             .catch(() => {
                 this._appUIService.showSnackbar('Message send error!', 'failure');
+                this.chatTranscripts.map(t => {
+                    if (t.messageId === messageId) {
+                        t.status = 'failed';
+                    }
+                });
             });
 
         // Reset the reply form
@@ -1760,7 +1792,7 @@ export class TwChatControlsComponent extends TWidgetWrapper implements OnInit, O
     pushToTranscript(transcript: ChatTranscripts): void {
         // check for message has link
         if (transcript.message) {
-
+            transcript.message = urlify(transcript.message);
         }
         this.chatTranscripts.push(transcript);
     }
