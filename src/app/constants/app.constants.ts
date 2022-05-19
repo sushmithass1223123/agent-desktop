57cf2245f0aff11091da2e01d1fa3816c44fac20
sirajuddin.m <sirajuddin.m@tetherfi.com>
2022-05-19 16:11:23 +0530

    🔨 fix (ActiveAgentsWidget): Some of the supervisor action buttons were not displaying as expected

--- src/app/constants/app.constants.ts
+++ src/app/constants/app.constants.ts
@@ -294,13 +294,13 @@ export const AGENT_FEATURES_MAP = {
     },
     AllowSupervisorToCapturePicture: {
         Type: 'agent',
-        SubType: [],
+        SubType: ['all'],
         Icon: 'fact_check',
         Label: 'View Activity'
     },
     AllowSupervisorToChangeStatus: {
         Type: 'agent',
-        SubType: [],
+        SubType: ['all'],
         Icon: 'track_changes',
         Label: 'Change Status'
     },
@@ -342,13 +342,13 @@ export const AGENT_FEATURES_MAP = {
     },
     AllowSupervisorToLogout: {
         Type: 'agent',
-        SubType: [],
+        SubType: ['all'],
         Icon: 'power_settings_new',
         Label: 'Logout'
     },
     AllowSupervisorToSendNotification: {
         Type: 'agent',
-        SubType: [],
+        SubType: ['all'],
         Icon: 'notifications',
         Label: 'Send Notification'
     },
@@ -360,7 +360,7 @@ export const AGENT_FEATURES_MAP = {
     },
     AllowSupervisorToViewEmailDetails: {
         Type: 'interaction',
-        SubType: [],
+        SubType: ['all'],
         Icon: 'email',
         Label: 'View Details'
     }
