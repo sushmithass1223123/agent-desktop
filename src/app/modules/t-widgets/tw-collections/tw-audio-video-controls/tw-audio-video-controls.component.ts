99030ab4e1b78ffb75a0a24cd481f5e9036a74c8
Sharan Puthran <varsha.b@tetherfi.com>
2022-11-14 16:31:51 +0530

    feat(AudioVideoControlsWidget): To save AV Call status results

--- src/app/modules/t-widgets/tw-collections/tw-audio-video-controls/tw-audio-video-controls.component.ts
+++ src/app/modules/t-widgets/tw-collections/tw-audio-video-controls/tw-audio-video-controls.component.ts
@@ -646,6 +646,7 @@ export class TwAudioVideoControlsComponent extends TWidgetWrapper implements OnI
                     break;
                 case 'onAVStats':
                     this.status = evt.data;
+                    this.sendAVStatusToServer();
                     break;
                 case 'onConnected':
                     this.connected = true;
@@ -700,6 +701,7 @@ export class TwAudioVideoControlsComponent extends TWidgetWrapper implements OnI
                     }
                     // push to the list
                     this.userList.push(evt.data);
+                    console.log('EVT', this.userList);
                     break;
                 case 'onScreenshareEnded':
                     this.status = 'screenshare-ended';
@@ -796,6 +798,25 @@ export class TwAudioVideoControlsComponent extends TWidgetWrapper implements OnI
             this.logger.error('Error in onAVEvent', error);
         }
     };
+
+    /**
+     * To send av status to the server for logging & reporting purpose 
+     */
+
+    sendAVStatusToServer() {
+        try{
+            const requestArgs = {
+                interactionId : this.interactionId.toString(),
+                type : 'avcallstatus',
+                message: JSON.stringify({
+                    param: this.status
+                })
+            };
+            SDKClient.sendAVControlMessage(requestArgs);
+        } catch(e) {
+            this.logger.error('Error occured on sending AV status to server', e, true);
+        }
+    }
 
     /**
      * To handles custom DisconnectAVEvent
