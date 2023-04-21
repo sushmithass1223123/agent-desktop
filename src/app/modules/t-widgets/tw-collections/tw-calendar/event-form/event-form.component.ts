840ec7a5efd4a045d9fc65e813dc6b86eb96d03c
Anjalee Imalka <anjalee.imalka@tetherfi.com>
2023-04-21 12:25:10 +0530

    :bug: fix(CalendarEventsComponent): Restructured noWhiteSpaceValidator function

--- src/app/modules/t-widgets/tw-collections/tw-calendar/event-form/event-form.component.ts
+++ src/app/modules/t-widgets/tw-collections/tw-calendar/event-form/event-form.component.ts
@@ -204,7 +204,6 @@ export class CalendarEventFormDialogComponent implements OnInit, OnDestroy {
      */
     noWhitespaceValidator(control: FormControl) {
         const isWhitespace = (control.value || '').trim().length === 0;
-        const isValid = !isWhitespace;
-        return isValid ? null : { 'whitespace': true };
+        return !isWhitespace ? null : { 'whitespace': true };
     }
 }
