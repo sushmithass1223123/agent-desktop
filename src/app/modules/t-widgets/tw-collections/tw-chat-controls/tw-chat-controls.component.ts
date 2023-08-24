9c8de805239e307e805a90d58ea92b2fb92ab63b
gimhana.d <gimhana.d@tetherfi.com>
2023-08-24 09:52:20 +0530

    :bug: fix(twChatControls): Fixed an issue that caused the video sent by the agent to have empty space around the video when the agent previewed it.

--- src/app/modules/t-widgets/tw-collections/tw-chat-controls/tw-chat-controls.component.ts
+++ src/app/modules/t-widgets/tw-collections/tw-chat-controls/tw-chat-controls.component.ts
@@ -898,7 +898,7 @@ export class TwChatControlsComponent extends TWidgetWrapper implements OnInit, O
                         }
                     ];
                     //displays toaster when a conference is disconnected
-                    this._appUIService.showSnackbar(this.getUpdatedLabel(this.translocoService.translate('widgets.chatControls.agentDisconnectedMsg'),dynamicLabels), 'info');
+                    this._appUIService.showSnackbar(this.getUpdatedLabel(this.translocoService.translate('widgets.chatControls.agentDisconnectedMsg'), dynamicLabels), 'info');
                     break;
             }
             return;
@@ -2662,6 +2662,22 @@ export class TwChatControlsComponent extends TWidgetWrapper implements OnInit, O
 
         this.previewMediaDialogRef = this._matDialog.open(this.previewMediaDialog, {
             panelClass: 'preview-media-dialog'
+        });
+        this.previewMediaDialogRef.afterOpened().subscribe(() => {
+            if (previewData.attachment.type === 'video') {
+                const scrollContainer = document.querySelector('.drag-scroll-content') as HTMLDivElement;
+                if (scrollContainer) {
+                    scrollContainer.style.overflow = 'auto';
+                    scrollContainer.style.height = '';
+                    scrollContainer.style.height = '100%';
+                    scrollContainer.style.minHeight = '100px';
+                    scrollContainer.style.maxHeight = '600px';
+                    scrollContainer.style.width = '';
+                    scrollContainer.style.maxWidth = '800px';
+                    scrollContainer.style.display = 'flex';
+
+                }
+            }
         });
     }
 
