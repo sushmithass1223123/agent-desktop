4f8b889cc9d8acf6d8b4a21de9237e6856dda27d
Yashwanthkumar Arivazhagan <yashwanthkumar.arivazhagan@tetherfi.com>
2024-02-12 12:02:02 +0530

    🔨 fix(TwVoiceControls - PB-3404): Prevent creating AV connection on call-connecting event if MediaServerConferenceEnabled is enabled from configurations

--- src/app/modules/t-widgets/tw-collections/tw-voice-controls/tw-voice-controls.component.ts
+++ src/app/modules/t-widgets/tw-collections/tw-voice-controls/tw-voice-controls.component.ts
@@ -786,7 +786,7 @@ export class TwVoiceControlsComponent extends TWidgetWrapper implements OnInit, 
     /**
      * To handle conference mixer
      */
-    private handleConferenceMixer(msConferenceEnabled: boolean | undefined): void {
+    private handleConferenceMixer(): void {
         // get the main line and conference line
         const mainLine = this.getAVConnection(this.callLines[0]);
         // this.avConns[this.callLines[0]];
@@ -798,9 +798,6 @@ export class TwVoiceControlsComponent extends TWidgetWrapper implements OnInit, 
             this._appUIService.showSnackbar(this.translocoService.translate('widgets.voiceControls.conferenceError'));
             return;
         }
-
-        // If conference mixing to be done from Media Server, prevent mixing from CallSDK
-        if(msConferenceEnabled && this.widgetData.Conference?.MediaServerConferenceEnabled) return;
         
         // add the conference peer connection to the conference
         mainLine.addConference(conferenceLine.getPeerConnection());
@@ -1176,7 +1173,10 @@ export class TwVoiceControlsComponent extends TWidgetWrapper implements OnInit, 
                 connection.unHold();
             }
             // // do conference mixing
-            this.handleConferenceMixer(evt?.MediaServer_ConferenceEnabled);
+            if(this.widgetData.Conference?.MediaServerConferenceEnabled && evt?.MediaServer_ConferenceEnabled) {
+                this.handleConferenceMixer();
+                this.logger.debug(`Conference mixing from Agent Desktop`, true);
+            } else this.logger.debug(`Conference mixed by Media Server`, true);
         }
         // else {
         // set the status
@@ -1217,8 +1217,10 @@ export class TwVoiceControlsComponent extends TWidgetWrapper implements OnInit, 
                     break;
                 case 'call-connecting':
                     // create WebRTC peer connection
-                    connection = this.createAVConnection(evt.SessionID, 'out');
-                    connection?.directCall(TEnums.WrcCallTypes.Audio);
+                    if(!this.widgetData.Conference?.MediaServerConferenceEnabled) {
+                        connection = this.createAVConnection(evt.SessionID, 'out');
+                        connection?.directCall(TEnums.WrcCallTypes.Audio);
+                    };
                     // play incoming call sound
                     this._appUIService.playAudio('ringing', 0.5, true);
                     break;
