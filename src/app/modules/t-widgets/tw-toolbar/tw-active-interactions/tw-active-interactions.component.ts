6259d6aa595983655a026cfef2b1a6796c525efb
sushmithashetty1234567 <156498038+sushmithashetty1234567@users.noreply.github.com>
2024-05-23 09:05:05 +0530

    :bug: fix(activeinteractioncomponenet): BEA - Redial function is not working on selected number in the ADPOMWidget

--- src/app/modules/t-widgets/tw-toolbar/tw-active-interactions/tw-active-interactions.component.ts
+++ src/app/modules/t-widgets/tw-toolbar/tw-active-interactions/tw-active-interactions.component.ts
@@ -10,6 +10,7 @@ import { TWidgetWrapper } from '@twidgets/utils';
 import { InteractionRef } from 'app/interfaces';
 import { takeUntil } from 'rxjs/operators';
 import { TranslocoService } from '@ngneat/transloco';
+import { TMACEventService } from '@services/tmac-event.service';
 
 /**
  * Active interactions
@@ -48,7 +49,8 @@ export class TwActiveInteractionsComponent extends TWidgetWrapper implements OnI
         private _contentPageService: ContentPageService,
         private _fuseProgressBarService: FuseProgressBarService,
         private _appUIService: AppUiService,
-        private translocoService: TranslocoService
+        private translocoService: TranslocoService,
+        private _tmacEventService: TMACEventService
     ) {
         super('TwActiveInteractionsComponent');
     }
@@ -75,6 +77,16 @@ export class TwActiveInteractionsComponent extends TWidgetWrapper implements OnI
         this._contentPageService.mode.pipe(takeUntil(this.unsubscribeAll)).subscribe((mode: string) => {
             this.currentViewMode = mode;
         });
+                // subscribe to UI control events
+                this._tmacEventService.getUIControlEvents.pipe(takeUntil(this.unsubscribeAll)).subscribe((data) => {
+                    try {
+                      if (data && data.interactionId)  {
+                        this.handleUIControls(data);
+                      }
+                    } catch (e) {
+                      console.log('Error occurred on UIControl event received', e);
+                    }
+                  });
     }
 
     /**
@@ -85,7 +97,18 @@ export class TwActiveInteractionsComponent extends TWidgetWrapper implements OnI
         // call the wrapper destroy method
         this.destroyWrapper();
     }
-
+    /**
+     * Method to manipulate interaction controls based on the custom events
+     * @param data 
+     */
+    handleUIControls(data:any): void {
+        if (data.eventName === 'changePhoneNumber') {
+            const index = this.interactionList.findIndex(item => item.interactionId === data.interactionId);
+            if (index !== -1) {
+                this.interactionList[index].user = data.phoneNumber;
+            }
+        }
+    }
     /**
      * Trackby for mat tree node
      * @param _index
