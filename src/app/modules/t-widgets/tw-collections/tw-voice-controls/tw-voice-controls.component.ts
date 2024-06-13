0cfa228375b166a665c652b7353e4d87df85506a
carol <carol.mendonca@tetherfi.com>
2024-06-13 09:38:12 +0530

    :bug: fix(PQ-956 - twVoiceControlsComponent): Adding appropriate message
    Adding appropriate message

--- src/app/modules/t-widgets/tw-collections/tw-voice-controls/tw-voice-controls.component.ts
+++ src/app/modules/t-widgets/tw-collections/tw-voice-controls/tw-voice-controls.component.ts
@@ -1655,8 +1655,8 @@ export class TwVoiceControlsComponent extends TWidgetWrapper implements OnInit, 
             // check for the response
             if (dt.response && dt.response.ResultCode === 0) {
                 // disconnect call success
-            } else if(dt.response.ResultCode == -3){
-                this._appUIService.showSnackbar(this.translocoService.translate('interactionComponent.unHoldCallFailed'), 'failure');
+            } else if (dt.response.ResultCode == -33){
+                this._appUIService.showSnackbar(this.translocoService.translate('widgets.voiceControls.resourseBusyError'), 'failure');
             } else {
                 this._appUIService.showSnackbar(this.translocoService.translate('interactionComponent.unHoldCallFailed'), 'failure');
             }
