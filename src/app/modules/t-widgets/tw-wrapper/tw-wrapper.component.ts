6f8fff217e69d5cd5f350aeff15fa4cf5000bb14
sushmithashetty1234567 <156498038+sushmithashetty1234567@users.noreply.github.com>
2024-06-26 17:39:21 +0530

    :bug: fix(tw-wrappercomponent):  in AD for Windows that allows bringing a particular window to the front by clicking on it? Currently, we can't pull it to the front; dragging is required

--- src/app/modules/t-widgets/tw-wrapper/tw-wrapper.component.ts
+++ src/app/modules/t-widgets/tw-wrapper/tw-wrapper.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, HostBinding, Input, OnDestroy, OnInit, Output, ViewEncapsulation } from '@angular/core';
+import { Component, EventEmitter, HostBinding, HostListener, Input, OnDestroy, OnInit, Output, ViewEncapsulation } from '@angular/core';
 import { AppUiService } from '@services/app-ui.service';
 import { FuseFacadeService } from '@services/fuse-facade.service';
 import { TMACEventService } from '@services/tmac-event.service';
@@ -106,12 +106,28 @@ export class TwWrapperComponent implements OnInit, OnDestroy {
      * Flag to check interaction on hold
      */
     interactionHold: boolean;
+    /**
+     * Host class for style z-index
+     */   
+    @HostBinding('style.z-index') 
+    
+    /**
+     * index to drag
+     */
+     zIndex: number;
 
+    /**
+     * index to drag and stay there
+     */
+    private static zIndexCounter = 1000; // Static counter for z-index
     /**
      * Unsubscribe all subject
      */
     _unsubscribeAll: Subject<any>;
-
+    /**
+     * Initially while dragging floated thing
+     */
+    isDragging = false;
     /**
      * Fuse custom config
      */
@@ -256,6 +272,20 @@ export class TwWrapperComponent implements OnInit, OnDestroy {
                 this.interactionHold = false;
             }
          }, 50);
- 
+    }
+
+    @HostListener('mousedown', ['$event'])
+    onDragStart(event: MouseEvent): void {
+        this.isDragging = true;
+        this.bringToFront();
+    }
+
+    @HostListener('mouseup', ['$event'])
+    onDragEnd(event: MouseEvent): void {
+        this.isDragging = false;
+    }
+
+    private bringToFront(): void {
+        this.zIndex = ++TwWrapperComponent.zIndexCounter;
     }
 }
