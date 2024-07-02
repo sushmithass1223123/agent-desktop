45e1d9636c7a25eecccb1d5f9dda31efc736eb9c
carol <carol.mendonca@tetherfi.com>
2024-07-02 16:53:27 +0530

    :bug: fix(tw-audio-video-component): PB-4980 Switch Channel
    VIVR - WebRTC Stats data is missing for customer end

--- src/app/modules/t-widgets/tw-collections/tw-audio-video-controls/tw-audio-video-controls.component.ts
+++ src/app/modules/t-widgets/tw-collections/tw-audio-video-controls/tw-audio-video-controls.component.ts
@@ -1477,6 +1477,8 @@ if(evt.User !== this.user.agentId && evt.User !== 'customer') return;
             } else if (this.muteAVOnHold.customerVideo) {
                 type = 'video';
                 actionMessage.type = 'muteVideo';
+            } else {
+                return;
             }
 
             this.requestMuteUnmuteCustomerAV(type, 'mute', actionMessage);
