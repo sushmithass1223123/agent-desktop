cf68be7d8c555ffae72c687997ec70c980936a9a
Varsha <varsha.b@tetherfi.com>
2024-07-26 20:04:03 +0530

    {icon}fix(DeflectToDigital): Error was shown for SMS number not provided when trying to send email

--- src/app/modules/t-widgets/tw-collections/tw-deflect-to-digital/tw-deflect-to-digital.component.ts
+++ src/app/modules/t-widgets/tw-collections/tw-deflect-to-digital/tw-deflect-to-digital.component.ts
@@ -75,6 +75,8 @@ export class TwDeflectToDigitalComponent extends TWidgetWrapper implements OnIni
         if (!this.data.Data.Number?.toLowerCase().includes('event')) {
             return;
         }
+        // setting default value for type is SMS
+        this.sendType.setValue(this.SendTypeList[0].value);
 
         const eventName = this.data.Data.Number?.split('.')?.shift() as any;
         this._agentFeaturesService.features.pipe(takeUntil(this.unsubscribeAll)).subscribe((change: boolean) => {
@@ -133,14 +135,15 @@ export class TwDeflectToDigitalComponent extends TWidgetWrapper implements OnIni
      */
     async sendTemplate(template: string): Promise<void> {
         try {
+            this.interactionId = 10001;
             // check if any interaction is present
             if (!this.interactionId) {
                 return;
             }
 
-            if (!this.toNumber) {
-                this._appUIService.showSnackbar(this.translocoService.translate('widgets.deflectToDigital.toFieldRequiredMsg'), 'failure');
-                return;
+            if(this.sendType.value === 'sms' && !this.toNumber) {
+                    this._appUIService.showSnackbar(this.translocoService.translate('widgets.deflectToDigital.toFieldRequiredMsg'), 'failure');
+                    return;
             }
 
             if(this.sendType.value === 'email' && this.emailId.invalid) {
