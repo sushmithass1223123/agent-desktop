dc237101a0fd441d5334745c268e7d814abaf6dc
sushmithashetty1234567 <156498038+sushmithashetty1234567@users.noreply.github.com>
2024-07-29 11:20:53 +0530

    :bug: fix(agent skill list component): EMM : Unable to Transfer Email to Another Agent via Skill Transfer by Manual Entry

--- src/app/modules/shared/components/agent-skill-list/agent-skill-list.component.ts
+++ src/app/modules/shared/components/agent-skill-list/agent-skill-list.component.ts
@@ -1445,7 +1445,19 @@ export class AgentSkillListComponent implements OnInit, AfterViewInit, OnDestroy
         this.selectedItem = '';
         this.clearDisplayValues();
     }
-
+    /**
+     * To select skill id on manual typing
+     */
+    onSkillVdnInputChange(value: string): void {
+        if (value.length === 5) { 
+          const skill = this.allSkills.find(s => s.SkillID.toString() === value);
+          if (skill) {
+            this.selectedSkill = skill.SkillID;
+            this.selectedItemDisplayName = skill.SkillName;
+            this.selectedRow = { type: 'Skill List', row: skill };
+          }
+        }
+      }
     /**
      * Load speed dial table
      */
