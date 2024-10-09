167360513727e4acc6b774beb186778398c46f1d
Yashwanthkumar Arivazhagan <tetherfi@Tetherfis-MacBook-Pro.local>
2024-10-09 12:08:16 +0530

    🔨 fix(PQ-1038 - TwEmailControls | EmmConstants): Identifying maker queue as outbox reason results in UI info issues

--- src/app/constants/emm.constants.ts
+++ src/app/constants/emm.constants.ts
@@ -1,5 +1,6 @@
 // route reasons
-export const OUTBOX_REASONS = ['CheckerQueue', 'CheckerPull', 'MakerQueue'];
+export const OUTBOX_REASONS = ['CheckerQueue', 'CheckerPull'];
+export const REJECT_REASONS = ['MakerQueue'];
 export const SENT_REASONS = ['AgentSentPull'];
 export const DRAFT_REASONS = ['AgentDraftPull'];
 export const INBOX_REASONS = ['MakerQueue', 'AgentPull', 'TransferToAgent', 'TransferToSkill'];
