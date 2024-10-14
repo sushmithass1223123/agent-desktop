b8c652986e97651aeb156f88a5b31ca40105743a
Varsha Bommarabettu <varsha.b@tetherfi.com>
2024-10-14 04:52:36 +0000

    Merge branch 'fix/PB-3980-Sprint13' into 'develop'
    :bug: fix( PB-3980-ChatControlComponent):AD-VIVR - no notification displayed regarding av call accepted/rejected by agent 2 in conference
    
    Closes PB-3980
    
    See merge request tetherfi_product/products/tmac/ui/agent-desktop!579
    
    (cherry picked from commit 61655cb4fbee129787a4c4d60907d5fb5fe1cb61)
    
    47711c28 :bug: fix(ChatControlComponent): PB-3980-AD

--- src/app/modules/t-widgets/tw-collections/tw-chat-controls/tw-chat-controls.component.ts
+++ src/app/modules/t-widgets/tw-collections/tw-chat-controls/tw-chat-controls.component.ts
@@ -2020,7 +2020,15 @@ export class TwChatControlsComponent extends TWidgetWrapper implements OnInit, O
         this.chatMode = evt.ChatMode as any;
         // Av call constraints from transfer notification event
         let avCallConstraints: any = {};
-        
+          // message to show for agent himself whe he is connected - "You have connected to chat"
+          this.pushToTranscript({
+            messageId: TUtils.Generic.uuid(),
+            message: this.translocoService.translate('widgets.chatControls.selfChatConnectedMsg'),
+            dividerMessage: true,
+            //if needed replace '-' with '/'
+            time: moment(new Date())
+        });
+
         if(this._tmacEventService.avCallConstraints[evt.TextChatIncomingEvent.SourceAgentID]) {
             avCallConstraints = JSON.parse(JSON.stringify(this._tmacEventService.avCallConstraints[evt.TextChatIncomingEvent.SourceAgentID]));
         }
@@ -2119,6 +2127,15 @@ export class TwChatControlsComponent extends TWidgetWrapper implements OnInit, O
                         value: evt.AgentName
                     }
                 ]
+                  //to show "You have joined the chat" when an agent joins to conference chat
+                  this.pushToTranscript({
+                    messageId: TUtils.Generic.uuid(),
+                    message: this.getUpdatedLabel(this.translocoService.translate('widgets.chatControls.chatConnectedMsg'), dynamicLabels),
+                    dividerMessage: true,
+                    //if needed replace '-' with '/'
+                    time: moment(new Date())
+                });
+
                 this._appUIService.showSnackbar(this.getUpdatedLabel(this.translocoService.translate('widgets.chatControls.chatConnectedMsg'), dynamicLabels), 'info');
             }
         } catch (error) { }
