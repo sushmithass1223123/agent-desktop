cdd5ec5d17b94c9e22c79c747463122257ab5395
sushmitha <sushmitha.shetty@tetherfi.com>
2024-10-22 11:43:32 +0530

    :bug: fix(chatcontrols-component): AD : When agent or customer rejects an escalated AV Call, then 'request av call' button gets disabled

--- src/app/modules/t-widgets/tw-collections/tw-chat-controls/tw-chat-controls.component.ts
+++ src/app/modules/t-widgets/tw-collections/tw-chat-controls/tw-chat-controls.component.ts
@@ -3151,30 +3151,30 @@ export class TwChatControlsComponent extends TWidgetWrapper implements OnInit, O
      * To escalate the chat to audio/video
      * @param {'audio' | 'video'} type Type of escalation
      */
-public escalateToAV(type: 'audio' | 'video', avCallConstraints?: any): void {
+    public escalateToAV(type: 'audio' | 'video', avCallConstraints?: any): void {
         // Check for parent agent av constraints
-    if (avCallConstraints === undefined) {
+        if (avCallConstraints === undefined) {
             avCallConstraints =
                 this._tmacEventService.avCallConstraints[
-            Object.keys(this._tmacEventService.avCallConstraints).find((parentAgentId: string) => {
-                return this._tmacEventService.avCallConstraints[parentAgentId].isAgentOnActiveCall;
-            })
-        ];
-    }
-    
+                    Object.keys(this._tmacEventService.avCallConstraints).find((parentAgentId: string) => {
+                        return this._tmacEventService.avCallConstraints[parentAgentId].isAgentOnActiveCall;
+                    })
+                ];
+        }
+        
         // open call widget
-    this.isCallWidgetOpened = true;
-    this.openCallWidget(type, 'out', avCallConstraints);
+        this.isCallWidgetOpened = true;
+        this.openCallWidget(type, 'out', avCallConstraints);
 
-    // this._tmacEventService.emitSDKEvent({
-    //     event: {
-    //         EventName: 'EscalateToAVEvent',
-    //         InteractionID: this.interaction.InteractionID,
-    //         CallType: type
-    //     },
-    //     isInteractionEvent: true
-    // });
-}
+        // this._tmacEventService.emitSDKEvent({
+        //     event: {
+        //         EventName: 'EscalateToAVEvent',
+        //         InteractionID: this.interaction.InteractionID,
+        //         CallType: type
+        //     },
+        //     isInteractionEvent: true
+        // });
+    }
 
     /**
      * To preview the media sent by customer or agent
