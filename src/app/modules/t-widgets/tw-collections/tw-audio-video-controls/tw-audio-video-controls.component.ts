0ce16f58c79539bf8f397f2e4576a9e59dc1db8e
carol <carol.mendonca@tetherfi.com>
2024-12-04 18:22:00 +0530

    :bug: fix(PB-9722 AudioVideoComponent): Customer name getting changed after Agent A transfer chat to Agent B
    Relase

--- src/app/modules/t-widgets/tw-collections/tw-audio-video-controls/tw-audio-video-controls.component.ts
+++ src/app/modules/t-widgets/tw-collections/tw-audio-video-controls/tw-audio-video-controls.component.ts
@@ -963,7 +963,7 @@ if (error === 'Screenshare Was Cancelled') {
                             };
                             // restricted source agent getting added to transferred call
                             // todo: this change should be handled at TMAC SDK side
-                            if(this.interactionDetails.ConferenceType === 'transfer' && newStreamObj.streamInfo.user.toLowerCase() !== 'customer') return;
+                            // if(this.interactionDetails.ConferenceType === 'transfer' && newStreamObj.streamInfo.user.toLowerCase() !== 'customer') return;
                             this.userList.push(newStreamObj);
                         })
                     }
