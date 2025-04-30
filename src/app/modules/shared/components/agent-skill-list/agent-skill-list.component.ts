1c8f259fb519c9c617bf16c01595832f56a57a27
Varsha <varsha.b@tetherfi.com>
2025-04-30 15:03:23 +0530

    {icon}fix(AgentSkillListComponent): Select skill issue fix

--- src/app/modules/shared/components/agent-skill-list/agent-skill-list.component.ts
+++ src/app/modules/shared/components/agent-skill-list/agent-skill-list.component.ts
@@ -1893,6 +1893,7 @@ export class AgentSkillListComponent implements OnInit, AfterViewInit, OnDestroy
                     }
                     // assign the selected row
                     this.selectedRow = { type: 'Skill List', row };
+                    if(this.allowDirectEdit) this.updateFreetextValue();
                 } else {
                     this._appUIService.showSnackbar(this.translocoService.translate('sharedComponents.agentSkillList.getSkillFailed'), 'failure');
                     row.Stf = 'NA';
@@ -1907,8 +1908,6 @@ export class AgentSkillListComponent implements OnInit, AfterViewInit, OnDestroy
                 row.CIQ = 'NA';
                 this.loading -= 1;
             });
-        
-        if(this.allowDirectEdit) this.updateFreetextValue();
     };
 
     skillSelecteFailed(message: string) {
