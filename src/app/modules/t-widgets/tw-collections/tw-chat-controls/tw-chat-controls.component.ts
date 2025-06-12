ae1c73a910811bd47e053da5e2c46f533a7a1a4b
Yashwanthkumar Arivazhagan <yashwanthkumar.arivazhagan@tetherfi.com>
2025-06-12 16:58:55 +0530

    Styles and Widget closing issues!

--- src/app/modules/t-widgets/tw-collections/tw-chat-controls/tw-chat-controls.component.ts
+++ src/app/modules/t-widgets/tw-collections/tw-chat-controls/tw-chat-controls.component.ts
@@ -1787,10 +1787,15 @@ export class TwChatControlsComponent extends TWidgetWrapper implements OnInit, O
         const widgetId = TUtils.Generic.uuid();
 
         widget.InteractionDetails = this.data.InteractionDetails;
-        // this.isMobileDevice = this.customerDevice;
-        widget.Data = { ...this.data.Data, ChatMode: this.chatMode,
-            IsScreenShareDisabled: (this.customerDevice || this.socialMedia) 
-            && this.DisableAvConstraints?.RequestScreenShare, AvCallConstraints };
+        widget.Data = {
+            ...this.data.Data,
+            ChatMode: this.chatMode,
+            IsScreenShareDisabled:
+                (this.customerDevice || this.socialMedia) && this.DisableAvConstraints?.RequestScreenShare,
+            AvCallConstraints,
+            StandaloneAv: this.StandaloneAv,
+            StandaloneAvId: widgetId
+        };
         widget.Data.Source = 'TwChatControlsComponent';
         widget.Data.CallType = param;
         widget.Data.Direction = direction;
